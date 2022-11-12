4a66d7951aea2e601db78750f7348318c0ad1ac5
tobi turner <tobi@something.com>
2022-11-12 19:20:17 +1100

    added a lot of functionality 7:20PM 11/12/2022

--- README.md
+++ README.md
@@ -5,11 +5,29 @@
 ## Description
 
 This is a website I use to store all the cheatsheets I have found on the internet, courses and other sources.  
-*the resources include **pdfs** and other **websites** both are **embedded** into this website*
+_the resources include **pdfs** and other **websites** both are **embedded** into this website_
 
 ### Dependencies
 
 - Uses adobe pdf API
+
+### References
+
+- big help: How Can I Pass a String Variable to an Anonymous Function?
+
+  - https://www.sitepoint.com/community/t/how-can-i-pass-a-string-variable-to-an-anonymous-function/352608/2
+
+- How To Zoom on Hover with CSS (w3schools.com)
+
+  - https://www.w3schools.com/howto/howto_css_zoom_hover.asp
+
+- Fill background color left to right CSS - Stack Overflow
+
+  - https://stackoverflow.com/questions/17212094/fill-background-color-left-to-right-css
+  - _you have to adjust to make it top to bottom_
+
+- How to highlight text
+  - https://www.w3schools.com/howto/howto_css_disable_text_selection.asp
 
 ### Languages
 
@@ -18,4 +36,5 @@ This is a website I use to store all the cheatsheets I have found on the interne
 - CSS
 
 #### Other
+
 I also practiced using grids in this website
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,130 @@
+"strict";
+var adobeReady = false;
 document.addEventListener("adobe_dc_view_sdk.ready", function () {
-  var adobeDCView = new AdobeDC.View({ clientId: "56bd9cfc05f9496bb75307708a493ca3", divId: "adobe-dc-view" });
-  adobeDCView.previewFile({
-    content: { location: { url: "cheatsheets/css-cheatsheet.pdf" } },
-    metaData: { fileName: "css-cheatsheet.pdf" },
-  });
+  adobeReady = true;
 });
+
+// copied from somewhere (written in references)
+function callAdobeApi(PDFObject) {
+  if (!adobeReady) {
+    // Adobe SDK not ready yet. Try again in 200ms.
+    setTimeout(function () {
+      changeDoc();
+    }, 200);
+
+    return;
+  }
+  var adobeDCView = new AdobeDC.View({ clientId: "xxxx", divId: "adobe-dc-view" });
+  adobeDCView.previewFile(
+    {
+      content: {
+        location: {
+          url: PDFObject.url,
+        },
+      },
+      metaData: {
+        fileName: PDFObject.fileName,
+      },
+    },
+    {
+      embedMode: "FULL_WINDOW",
+      defaultViewMode: "FIT_WIDTH",
+    }
+  );
+}
+
+let uniqueId = 0;
+
+class PDF {
+  constructor(title, author, type, url, fileName) {
+    this.title = title;
+    this.author = author;
+    this.type = type;
+    this.id = uniqueId++;
+    this.url = url;
+    this.fileName = fileName;
+  }
+}
+
+const PDFCollection = [
+  new PDF("Simple JavaScript Cheatsheet", "iloveCoding", "javascript", "cheatsheets/JavaScript/js-cheatsheet-2.pdf", "js-cheatsheet-2.pdf"),
+  new PDF("Beginner's Essential", "WebsiteSetup", "javascript", "cheatsheets/JavaScript/JS_Cheat_1667421241.pdf", "JS_Cheat_1667421241.pdf"),
+  new PDF("Complete JavaScript CheatSheet", "PAPA REACT", "javascript", "cheatsheets/JavaScript/JS__1667799823.pdf", "JS__1667799823.pdf"),
+];
+
+console.log(PDFCollection[0].title);
+
+let openSheetClicked = false;
+let openPdfClicked = false;
+let prevPDFId = null;
+
+const openPDF = (e) => {
+  // get the pdf object selected
+  const PDFObject = e.currentTarget.PDFObject;
+  const cheatsheetsItem = e.currentTarget;
+  const prevPDFObjectId = e.currentTarget.prevPDFObjectId;
+  console.log(prevPDFObjectId);
+  if (!openPdfClicked) {
+    // make the pdf element
+    createPDFElements();
+  } else {
+    document.querySelectorAll("#adobe-dc-view").forEach((el) => {
+      el.remove();
+    });
+    if (prevPDFId !== PDFObject) {
+      createPDFElements();
+    } else {
+      openPdfClicked = false;
+    }
+  }
+  function createPDFElements() {
+    const pdfElement = document.createElement("div");
+    // give pdfElement the id
+    pdfElement.id = "adobe-dc-view";
+    // put pdf element after clicked cheatsheetItem
+    cheatsheetsItem.after(pdfElement);
+    // pass the current PDFObject
+    callAdobeApi(PDFObject);
+    openPdfClicked = true;
+  }
+  prevPDFId = PDFObject;
+};
+
+const openSheet = (clickedId) => {
+  console.log(clickedId);
+  // you can't click the button again
+  if (!openSheetClicked) {
+    // retrieve selected div element (or the div of the button that was clicked)
+    const clickedElement = document.querySelector(`#${clickedId}`);
+    for (let i = 0; i < PDFCollection.length; i++) {
+      // find pdfs that matched the selected div
+      if (PDFCollection[i].type === clickedId) {
+        // create div inside of the cheatsheet item for that element
+        const cheatsheetsItem = document.createElement("div");
+        // give it a class
+        cheatsheetsItem.classList.add("cheatsheetItem");
+        // add event listener
+        cheatsheetsItem.addEventListener("click", openPDF);
+        // make a parameter called PDFObject and pass the current PDFObject
+        cheatsheetsItem.PDFObject = PDFCollection[i];
+        // also pass the previous object id
+        cheatsheetsItem.prevPDFObjectId = PDFCollection[i];
+
+        // give an ID to each individual PDF
+
+        // make html to be appended to the div
+        cheatsheetsItem.innerHTML = `<h3>${PDFCollection[i].title}</h3>
+        <h4><i>by ${PDFCollection[i].author}</i></h4>
+        `;
+        // add the new elements after the clicked element
+        clickedElement.after(cheatsheetsItem);
+      }
+    }
+    openSheetClicked = true;
+  } else {
+    document.querySelectorAll(".cheatsheetItem").forEach((el) => {
+      el.remove();
+    });
+    openSheetClicked = false;
+  }
+};
